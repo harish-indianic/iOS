2f8a87f1173a5caed9531fec3c847936f4042520
indianic <shraddha.hattimare@indianic.com>
2014-09-16 17:55:06 +0530

    Initial

---
+++ iOS/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iOS
+//
+//  Created by IndiaNIC on 16/09/14.
+//  Copyright (c) 2014 IndiaNIC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iOS/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iOS
+//
+//  Created by IndiaNIC on 16/09/14.
+//  Copyright (c) 2014 IndiaNIC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
